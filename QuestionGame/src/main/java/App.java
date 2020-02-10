6781c47b6333a6bc9128e0d480ab19ef30c2149a
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-09 22:37:17 -0500

    Collect user input

--- QuestionGame/src/main/java/App.java
+++ QuestionGame/src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
@@ -11,7 +13,17 @@ public class App {
 
         System.out.println("TWO QUESTIONS!\n" +
                 "Think of an object, and I'll try to guess it.");
+    }
 
+    public static String collectUserInput(){
+
+        System.out.println("Question 1) Is it animal, vegetable, or mineral?");
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine().toLowerCase();
+        System.out.println(input);
+        return  input;
     }
 
 }
