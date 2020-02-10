703661d95a0bac5f2d365b8a7c1c80fb81b2e5f7
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-09 22:29:31 -0500

    Display message

---
+++ QuestionGame/src/main/java/App.java
@@ -0,0 +1,17 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        displayMessage();
+
+
+    }
+
+    public static void displayMessage(){
+
+        System.out.println("TWO QUESTIONS!\n" +
+                "Think of an object, and I'll try to guess it.");
+
+    }
+
+}
